695a9587fe3efb2044b5b5f0a38c05de56eff0a9
dimasbr89 <dimasbudiriansyah@gmail.com>
2025-02-19 15:37:31 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Blog from "@/components/Blog";
+import Brands from "@/components/Brands";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Testimonials from "@/components/Testimonials";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dimas BR | Your Consultant",
+  description:
+    "Dimas BR is an appraisal consultant, IT Consultant who helps you to grow your business.",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      <Pricing />
+      <Blog />
+      <Contact />
+    </>
+  );
+}
